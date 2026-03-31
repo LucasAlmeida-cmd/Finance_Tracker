4d88af3598a3a9ffe0f525f2f362fb5c2e8a207b
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2026-03-31 08:32:51 -0300

    feat: Criação classe user + role

---
+++ user-service/src/main/java/com/example/user_service/model/User.java
@@ -0,0 +1,39 @@
+package com.example.user_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "tb_user")
+@Setter
+@Getter
+@DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "nome_user", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "email_user", nullable = false, length = 100)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Roles role;
+
+    public User() {
+    }
+
+
+//    @Column(name = "cpf_user", nullable = false, length = 100)
+//    private String cpf;
+//    private LocalDate data_aniversario;
+}
