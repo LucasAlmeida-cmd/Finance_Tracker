d210f68b58b376f31a2e29a49121dd898ff48a2c
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2026-04-07 10:51:34 -0300

    feat: criao de entity e enum

---
+++ transaction-service/src/main/java/com/example/transaction_service/entity/Transaction.java
@@ -0,0 +1,49 @@
+package com.example.transaction_service.entity;
+
+import com.example.transaction_service.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
---
+++ transaction-service/src/main/java/com/example/transaction_service/enums/TransactionType.java
@@ -0,0 +1,5 @@
+package com.example.transaction_service.enums;
+
+public enum TransactionType {
+    INCOME, EXPENSE
+}
